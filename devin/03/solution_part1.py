cebb76286704502febebd6a4d2374829133d6b06
thedevinyang <30394453+thedevinyang@users.noreply.github.com>
2024-12-03 23:17:31 -0800

    Removed redundant code

--- devin/03/solution_part1.py
+++ devin/03/solution_part1.py
@@ -3,19 +3,17 @@ import re
 
 # Read the input file
 with open('input.txt', 'r') as file:
-    raw = file.readlines()
+    raw = file.read()
 
 # Regex for finding the uncorrupted multiply functions
 pattern = r'mul\([0-9]+,[0-9]+\)'
-
 total = 0
-for line in raw:
-    # Finds all instances of the multiplcation function in the corrupted memory
-    commands = re.findall(pattern, line)
-    # Isolate the numbers found in the multiplcations functions and add the product to the total
-    for operation in commands:
-        operation = operation.split(',')
-        operation = [int(operation[0][4::]), int(operation[1][:-1:])]
-        total += operation[0] * operation[1]
+# Finds all instances of the multiplcation function in the corrupted memory
+commands = re.findall(pattern, raw)
+# Isolate the numbers found in the multiplcations functions and add the product to the total
+for operation in commands:
+    operation = operation.split(',')
+    operation = [int(operation[0][4::]), int(operation[1][:-1:])]
+    total += operation[0] * operation[1]
 
 print(total)
