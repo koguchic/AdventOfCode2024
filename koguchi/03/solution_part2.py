ade0a06e08097fb53d67ac8e8c4a8b2432a9f843
Christian <christian_j_koguchi@apple.com>
2024-12-05 19:05:48 -0800

    Add problem 3 part 2

---
+++ koguchi/03/solution_part2.py
@@ -0,0 +1,29 @@
+import re
+
+
+with open('input.txt', 'r') as f:
+    input_string = f.read()
+
+
+pattern = r'mul\(\d+,\d+\)|do\(\)|don\'t\(\)'
+matches = re.findall(pattern, input_string)
+pattern_extract = r"mul\((\d+),(\d+)\)"
+
+total = 0
+do = True
+for match in matches:
+    print(match)
+    if "don't" in match:
+        do = False
+    elif "do" in match:
+        do = True
+    else: 
+        m = re.match(pattern_extract, match)
+        if m:
+            a = int(m.group(1))
+            b = int(m.group(2))
+            if do:
+                product = a * b
+                total += product
+
+print(total) # 82857512
