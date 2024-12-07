97556cb7d15c481b87b9e1954d8f1bbd836a2192
Max <Mushst@users.noreply.github.com>
2024-12-06 18:01:22 -0800

    psuedocode alt solution if memory is not issue

---
+++ Max/01/solution_part1_alt.py
@@ -0,0 +1,6 @@
+# 1. find the max of both lists
+# 2. create 2 arrays of size found in 1
+# 3. knowing each number is an integer and could be a repeat, tally occurence in the array index. end up with 2 tallies
+# 4. two pointer approach from index 0 of each list till we get to the end. Keep track of running abs(sum)
+
+# brute force memory
