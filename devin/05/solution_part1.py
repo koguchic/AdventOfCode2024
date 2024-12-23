0ffb53da4e016a159cd95367a7f3b0ae1afeb715
thedevinyang <30394453+thedevinyang@users.noreply.github.com>
2024-12-22 22:30:14 -0800

    Day 5 Solution part 1

---
+++ devin/05/solution_part1.py
@@ -0,0 +1,50 @@
+# Day 5 - Part 1
+from collections import deque
+
+with open('input.txt', 'r') as file:
+    raw = file.readlines()
+
+# Separate the input into two sections - the rules and the updates
+rules = dict()
+updates = []
+second = False
+for line in raw:
+    if line == '\n':
+        second = True
+    else:
+        if second:
+            line = line.strip().split(',')
+            updates.append(line)
+        else:
+            line = line.strip().split('|')
+            if line[0] not in rules:
+                rules[line[0]] = [line[1]]
+            else:
+                rules[line[0]].append(line[1])
+
+solution = 0
+for update in updates:
+    queue = deque(update)
+    seen = set()
+    valid = True
+    while queue:
+        curr = queue.popleft()
+        # Forward check to see if all upcoming pages come after the current page
+        for val in queue:
+            if curr in rules:
+                if val not in rules[curr]:
+                    valid = False
+
+        # Backward check to see if all pages that have already been seen should not come after the current page
+        for val in seen:
+            if curr in rules:
+                if val in rules[curr]:
+                    valid = False
+                    
+        seen.add(curr)
+
+    # Add the middle value of the update if the update is valid
+    if valid:
+        solution += int(update[len(update)//2])   
+
+print(solution)
